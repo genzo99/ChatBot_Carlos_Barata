68a0ea2507039c8f51072479cdb86b80584db011
genzo99 <carlosmart99@gmail.com>
2026-06-01 12:45:28 +0100

    2
    Descomentar o código

--- app.py
+++ app.py
@@ -21,20 +21,27 @@ def obter_resposta(texto: str) -> str:
 
     # return f'Desculpa, não entendi a questão! {texto}'
 
-    # respostas = {
-    #     ('olá', 'boa tarde', 'bom dia'): 'Olá tudo bem!',
-    #     'como estás': 'Estou bem, obrigado!',
-    #     ('bye', 'adeus', 'tchau'): 'Gostei de falar contigo! Até breve...',
-    # }
+   respostas = {
+        ('olá', 'boa tarde', 'bom dia'): 'Olá tudo bem!',
+        'como estás': 'Estou bem, obrigado!',
+        'como te chamas?': 'O meu nome é: Bot :)',
+        'tempo': 'Está um dia de sol!',
+        ('bye', 'adeus', 'tchau'): 'Gostei de falar contigo! Até breve...',
+    }
 
-    # for chave, resposta in respostas.items():
-    #     if isinstance(chave, tuple):
-    #         if comando in chave:
-    #             return resposta
-    #     elif chave in comando:
-    #         return resposta
+    for chave, resposta in respostas.items():
+        if isinstance(chave, tuple):
+            if comando in chave:
+                return resposta
+        elif chave in comando:
+            return resposta
 
-    # return f'Desculpa, não entendi a questão! {texto}'
+    if 'horas' in comando:
+        return f'São: {datetime.now():%H:%M} horas'
+    if 'data' in comando:
+        return f'Hoje é dia: {datetime.now():%d-%m-%Y}'
+
+    return f'Desculpa, não entendi a questão! {texto}'
 
 
 def chat() -> None:
